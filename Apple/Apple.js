a70747f25b9e653e555ce5c91247452b4d46f5ae
joefoges <steams_knock.7t@icloud.com>
2025-06-16 19:52:10 -0400

    Update Apple.js

--- Apple/Apple.js
+++ Apple/Apple.js
@@ -1,13 +1,10 @@
 /* eslint-disable require-yield, eqeqeq */
-
 import {
   Sprite,
   Trigger,
-  Watcher,
   Costume,
-  Color,
-  Sound,
-} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+  Sound
+} from "https://unpkg.com/leopard@1.30.1/dist/index.esm.js";
 
 export default class Apple extends Sprite {
   constructor(...args) {
